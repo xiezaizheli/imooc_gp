8492cf0277d556cc8284482a955cf01cdd7e86ee
xiezaizheli <15000734841@163.com>
2017-07-16 13:46:45 +0800

    项目底部导航菜单开发

--- index.android.js
+++ index.android.js
@@ -8,24 +8,62 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  Image,
   Text,
   View
-} from 'react-native';
+  } from 'react-native';
+
+import TabNavigator from 'react-native-tab-navigator';
 
 export default class imooc_gp extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      selectedTab:'tb_popular',
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.android.js
-        </Text>
-        <Text style={styles.instructions}>
-          Double tap R on your keyboard to reload,{'\n'}
-          Shake or press menu button for dev menu
-        </Text>
+        <TabNavigator>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tb_popular'}
+            title="最热"
+            selectedTitleStyle ={{color:'red'}}
+            renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_polular.png')} />}
+            renderSelectedIcon={() => <Image style={[styles.image,{tintColor:'red'}]} source={require('./res/images/ic_polular.png')} />}
+            badgeText="1"
+            onPress={() => this.setState({ selectedTab: 'tb_popular' })}>
+            <View style={styles.page1}></View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tb_trending'}
+            title="趋势"
+            selectedTitleStyle ={{color:'yellow'}}
+            renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_trending.png')} />}
+            renderSelectedIcon={() => <Image style={[styles.image,{tintColor:'yellow'}]} source={require('./res/images/ic_trending.png')} />}
+            onPress={() => this.setState({ selectedTab: 'tb_trending' })}>
+            <View style={styles.page2}></View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tb_favorite'}
+            title="收藏"
+            selectedTitleStyle ={{color:'yellow'}}
+            renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_favorite.png')} />}
+            renderSelectedIcon={() => <Image style={[styles.image,{tintColor:'yellow'}]} source={require('./res/images/ic_favorite.png')} />}
+            onPress={() => this.setState({ selectedTab: 'tb_favorite' })}>
+            <View style={styles.page2}></View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'tb_my'}
+            title="我的"
+            selectedTitleStyle ={{color:'yellow'}}
+            renderIcon={() => <Image style={styles.image} source={require('./res/images/ic_my.png')} />}
+            renderSelectedIcon={() => <Image style={[styles.image,{tintColor:'yellow'}]} source={require('./res/images/ic_my.png')} />}
+            onPress={() => this.setState({ selectedTab: 'tb_my' })}>
+            <View style={styles.page2}></View>
+          </TabNavigator.Item>
+        </TabNavigator>
       </View>
     );
   }
@@ -34,20 +72,20 @@ export default class imooc_gp extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  page1:{
+    flex:1,
+    backgroundColor:'red'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+  page2:{
+    flex:1,
+    backgroundColor:'yellow'
   },
+  image:{
+    width:22,
+    height:22
+  }
 });
 
 AppRegistry.registerComponent('imooc_gp', () => imooc_gp);
--- index.ios.js
+++ index.ios.js
@@ -8,24 +8,42 @@ import React, { Component } from 'react';
 import {
   AppRegistry,
   StyleSheet,
+  Image,
   Text,
   View
 } from 'react-native';
 
+import TabNavigator from 'react-native-tab-navigator';
+
 export default class imooc_gp extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      selectedTab:'home',
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit index.ios.js
-        </Text>
-        <Text style={styles.instructions}>
-          Press Cmd+R to reload,{'\n'}
-          Cmd+D or shake for dev menu
-        </Text>
+        <TabNavigator>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'home'}
+            title="Home"
+            renderIcon={() => <Image source={require('./res/images/ic_polular.png')} />}
+            renderSelectedIcon={() => <Image source={require('./res/images/ic_polular.png')} />}
+            badgeText="1"
+            onPress={() => this.setState({ selectedTab: 'home' })}>
+            <View style={styles.page1}></View>
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'profile'}
+            title="Profile"
+            renderIcon={() => <Image source={require('./res/images/ic_trending.png')} />}
+            renderSelectedIcon={() => <Image source={require('./res/images/ic_trending.png')} />}
+            onPress={() => this.setState({ selectedTab: 'profile' })}>
+            <View style={styles.page2}></View>
+          </TabNavigator.Item>
+        </TabNavigator>
       </View>
     );
   }
@@ -38,16 +56,14 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
+  page1:{
+    flex:1,
+    backgroundColor:'red'
   },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  page2:{
+    flex:1,
+    backgroundColor:'yellow'
+  }
 });
 
 AppRegistry.registerComponent('imooc_gp', () => imooc_gp);
