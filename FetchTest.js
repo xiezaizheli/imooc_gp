1aecf0d38c7970b24ccc6c5b2e6b72597ecee14d
xiezaizheli <15000734841@163.com>
2017-07-18 00:12:57 +0800

    HttpUtils

--- FetchTest.js
+++ FetchTest.js
@@ -14,60 +14,88 @@ import {
 
 import NavigationBar from './NavigationBar';
 import Toast, {DURATION} from 'react-native-easy-toast';
-
+import HttpUtils from './HttpUtils'
 export default class FetchTest extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
-    this.state={
-      result:'',
+    this.state = {
+      result: '',
     }
   }
-  onLoad(url){
-    fetch(url)
-      .then((response) => response.json())
+
+  onLoad(url) {
+    /* fetch(url)
+     .then((response) => response.json())
+     .then(result => {
+     this.setState({
+     result:JSON.stringify(result)
+     });
+     })
+     .catch((error) => {
+     this.setState({
+     result:JSON.stringify(error)
+     });
+     });*/
+    HttpUtils.get(url)
       .then(result => {
         this.setState({
-          result:JSON.stringify(result)
-        });
+          result: JSON.stringify(result)
+        })
       })
-      .catch((error) => {
+      .catch(error => {
         this.setState({
-          result:JSON.stringify(error)
-        });
-      });
+          result: JSON.stringify(error)
+        })
+      })
   }
-  onSubmit(url,data){
-    fetch(url,{
-      method:'POST',
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json',
-      },
-      body:JSON.stringify(data)
-    })
-      .then((response) => response.json())
+
+  onSubmit(url, data) {
+    /*fetch(url,{
+     method:'POST',
+     headers: {
+     'Accept': 'application/json',
+     'Content-Type': 'application/json',
+     },
+     body:JSON.stringify(data)
+     })
+     .then((response) => response.json())
+     .then(result => {
+     this.setState({
+     result:JSON.stringify(result)
+     });
+     })
+     .catch((error) => {
+     this.setState({
+     result:JSON.stringify(error)
+     });
+     });*/
+    HttpUtils.post(url, data)
       .then(result => {
         this.setState({
-          result:JSON.stringify(result)
-        });
+          result: JSON.stringify(result)
+        })
       })
-      .catch((error) => {
+      .catch(error => {
         this.setState({
-          result:JSON.stringify(error)
-        });
-      });
+          result: JSON.stringify(error)
+        })
+      })
   }
+
   render() {
     return (
       <View style={styles.container}>
         <NavigationBar title="FetchTest"/>
         <Text
           style={styles.tips}
-          onPress={()=>this.onLoad('http://rap.taobao.org/mockjsdata/11793/test')}
+          onPress={() => this.onLoad('http://rap.taobao.org/mockjsdata/11793/test')}
         >获取数据</Text>
         <Text
           style={styles.tips}
-          onPress={()=>this.onSubmit('http://rap.taobao.org/mockjsdata/11793/submit',{userName:'小明',password:'123456'})}
+          onPress={() => this.onSubmit('http://rap.taobao.org/mockjsdata/11793/submit', {
+            userName: '小明',
+            password: '123456'
+          })}
         >提交数据</Text>
 
         <Text>返回结果：{this.state.result}</Text>
